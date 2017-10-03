6851735d46081b549c159790537a0449f6f676e4
Efim Poberezkin <efim@poberezkin.ru>
2017-10-03 16:27:20 +0400

    Added implementation for class Stat.

--- src/main/java/character/Stat.java
+++ src/main/java/character/Stat.java
@@ -1,4 +1,24 @@
 package character;
 
+/**
+ * Class for pairs of attributes and values.
+ */
 public class Stat {
+
+    private Attribute attribute;
+    private int value;
+
+    public Stat(Attribute attribute, int value) {
+        this.attribute = attribute;
+        this.value = value;
+    }
+
+    public Attribute getAttribute() {
+        return attribute;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
 }
