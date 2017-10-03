db153c303d9753907fd5f2df42e529d472c321d2
Efim Poberezkin <efim@poberezkin.ru>
2017-10-03 16:28:58 +0400

    Added implementation for class Character.

--- src/main/java/character/Character.java
+++ src/main/java/character/Character.java
@@ -1,4 +1,21 @@
 package character;
 
 public class Character {
+
+    private String name;
+    private Stat[] stats;
+
+    public Character(String name, Stat[] stats) {
+        this.name = name;
+        this.stats = stats;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Stat[] getStats() {
+        return stats;
+    }
+    
 }
--- src/main/java/character/Stat.java
+++ src/main/java/character/Stat.java
@@ -20,5 +20,5 @@ public class Stat {
     public int getValue() {
         return value;
     }
-    
+
 }
