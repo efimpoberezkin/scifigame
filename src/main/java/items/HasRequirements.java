ea203230375405fe3b3dd24a75f50321c84e93a9
Efim Poberezkin <efim@poberezkin.ru>
2017-10-03 21:15:53 +0400

    Added method signatures to interface HasRequirements.

--- src/main/java/items/HasRequirements.java
+++ src/main/java/items/HasRequirements.java
@@ -1,4 +1,13 @@
 package items;
 
+import character.AttributeRequirement;
+
+/**
+ * Interface for item classes that have requirements.
+ */
 public interface HasRequirements {
+
+    public AttributeRequirement[] getAttributeRequirements();
+    public int getLevelRequirement();
+
 }
